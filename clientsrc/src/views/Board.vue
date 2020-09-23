99458057303df0732b44f5476c48018e7f138648
MatthewHufft <matthufft777@gmail.com>
2020-09-23 11:40:35 -0600

    added edit form

--- clientsrc/src/views/Board.vue
+++ clientsrc/src/views/Board.vue
@@ -5,10 +5,25 @@
       {{board.title}}
     </h1> 
     <i class="fa fa-pencil text-warning"
-       aria-hidden="true"
-       @click="editToggle = !editToggle"
-       v-if="isCreator">
-       </i>
+       aria-hidden="true" 
+       @click="editToggle = !editToggle">
+      </i>
+      <form class="form-inline" @submit.prevent="editBoard" v-if="editToggle">
+              <input
+                type="text"
+                class="form-control"
+                aria-describedby="helpId"
+                v-model="boardData.title"
+              />
+              <input
+                type="text"
+                class="form-control"
+                aria-describedby="helpId"
+                v-model="boardData.body"
+              />
+              <button type="submit" class="btn btn-warning"><i class="fa fa-arrow-circle-right big-icon" aria-hidden="true"></i></button>
+          </form>
+
 
     <h4 v-if="board.description">{{board.description}}</h4>
     <form onsubmit.prevent="addList">
@@ -57,7 +72,13 @@ export default {
     return {newList: {
       title: '',
       boardId: this.$route.params.boardId
-    }}
+    },
+      boardData: {
+        title: '',
+        body: ''
+      },
+      editToggle: false
+    }
   },
   props: ["boardId"],
   components: {
